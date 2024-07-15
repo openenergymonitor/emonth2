9e45bd9700cdfb2d398ba563b8e20a73d3021872
tiberiusteng <tiberiusteng@users.noreply.github.com>
2024-07-15 21:38:02 +0900

    just cast to `int` is enough

--- firmware/emonth2.ino
+++ firmware/emonth2.ino
@@ -318,7 +318,7 @@ void setup()
       si7021_env data = SI7021_sensor.getHumidityAndTemperature();
       Serial.print("SI7021 Started, ID: ");
       Serial.println(deviceid); delay(100);
-      Serial.print("temp:"); Serial.print(data.celsiusHundredths/100.0); Serial.print(",humidity:"); Serial.println(((data.humidityBasisPoints+600)/100.0)-6);
+      Serial.print("temp:"); Serial.print(data.celsiusHundredths/100.0); Serial.print(",humidity:"); Serial.println((int(data.humidityBasisPoints)/100.0));
       delay(100);
     }
     else 
@@ -493,7 +493,7 @@ void loop()
       #endif
       si7021_env data = SI7021_sensor.getHumidityAndTemperature();
       emonth.temp = (data.celsiusHundredths*0.1);
-      emonth.humidity = (((data.humidityBasisPoints+600)*0.1)-60);
+      emonth.humidity = (int(data.humidityBasisPoints)*0.1);
       #ifndef ATTINY
       power_twi_disable();
       #endif
